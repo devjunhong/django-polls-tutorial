dad7e6a77bb400c23ae6d70dec96e42c1adee3be
junhongGenesislab <unique3801@gmail.com>
2020-03-31 06:08:31 +0900

    add test models

--- polls/models.py
+++ polls/models.py
@@ -15,6 +15,12 @@ class Question(models.Model):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
+    def question_and_date(self):
+        return "{} postead at {}".format(
+            self.question_text,
+            self.pub_date
+        )
+
     was_published_recently.admin_order_field = 'pub_date'
     was_published_recently.boolean = True 
     was_published_recently.short_description = 'Publisehd recently?'
@@ -27,3 +33,7 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
+
+    def add_vote(self):
+        self.votes += 1
+        self.save()
